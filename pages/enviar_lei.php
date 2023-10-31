045b11762916ee3461c320678b99accdc9c947a1
solitariewolf <watanasio@gmail.com>
2023-10-30 22:27:03 -0300

    Changes to be committed: 	modified:   pages/enviar_lei.php 	modified:   pages/enviar_lei_complementar.php

--- pages/enviar_lei.php
+++ pages/enviar_lei.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['textoLei'])) {
     $textoLei = $_POST['textoLei'];
 
     // SQL para inserir o texto da lei na tabela votacoes_leis
-    $sql = "INSERT INTO votacoes_leis (Texto_Original) VALUES (?)";
+    $sql = "INSERT INTO votacoes_leis (Texto_Original, Votos_Positivos, Votos_Negativos) VALUES (?, 0, 0)";
     
     // Preparar e executar a consulta
     $stmt = $conn->prepare($sql);
