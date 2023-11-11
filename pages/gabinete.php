defd700d690f16df3dd85abb640af8cc54f34c6b
solitariewolf <watanasio@gmail.com>
2023-11-11 20:06:42 -0300

    Changes to be committed: 	new file:   banco/transferir-governo.php 	modified:   loja/comprar.php 	modified:   pages/gabinete.php

--- pages/gabinete.php
+++ pages/gabinete.php
@@ -93,6 +93,7 @@ $resultitens = $conn->query($sql);
   <button class="tablinks" onclick="openCity(event, 'Tab1')">Leis e Decretos</button>
   <button class="tablinks" onclick="openCity(event, 'Tab2')">Medalhas</button>
   <button class="tablinks" onclick="openCity(event, 'Tab3')">Loja</button>
+  <button class="tablinks" onclick="openCity(event, 'Tab4')">Recursos</button>
 </div>
 
 <div id="Tab1" class="tabcontent">
@@ -518,6 +519,102 @@ $resultitens = $conn->query($sql);
             ?>
         </div><!--gabinete-geral-loja-->
     </div><!--tab3-->
+
+    <div id="Tab4" class="tabcontent">
+            <p>conteudo</p>
+
+            <div class="logo-banco"> <img src="img/banco/bank-logo.png" alt=""></div>
+
+<?php
+// Pega o ID do usuário da sessão
+$id_usuario = 1;
+
+// Prepara a consulta SQL
+$consulta = $conn->prepare("SELECT u.nome, b.money FROM usuarios u INNER JOIN banco b ON u.id = b.usuario WHERE u.id = ?");
+$consulta->bind_param('i', $id_usuario);
+
+// Executa a consulta
+$consulta->execute();
+
+// Fetch the result
+$resultado = $consulta->get_result()->fetch_assoc();
+
+// Formata o valor do dinheiro com pontos como separadores de milhares
+$money_formatado = number_format($resultado['money'], 2, ',', '.');
+
+// Exibe a mensagem
+echo "<p class='saldo-inicio'>Olá " . $resultado['nome'] . ", seu saldo é de MC$: " . $money_formatado . "</p>";
+?>
+<div class="duas-primeiras">
+
+<div class="form-transferir">
+<h1>Relizar transferência entre contas</h1>
+<form action="banco/transferir-governo.php" method="post">
+<label for="usuario">Usuário:</label><br>
+<select class="form-control bg-light rounded" name="usuario" id="usuario">
+    <option value="" disabled selected>Escolha um membro</option>
+    <?php
+        $sql = "SELECT id, nome FROM usuarios WHERE id != 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // Saída dos dados de cada linha
+            while($row = $result->fetch_assoc()) {
+                echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
+            }
+        } else {
+            echo "<option value=''>Nenhum usuário encontrado</option>";
+        }
+    ?>
+
+</select><br>
+<label for="valor">Valor:</label><br>
+<input type="number" id="valor" name="valor" min="0" step="0.01"><br>
+<label for="mensagem">Mensagem:</label><br>
+<textarea id="mensagem" name="mensagem"></textarea><br>
+<input type="submit" value="Transferir">
+</form>
+</div><!--form-transferir-->
+
+<div class="form-extrato">
+<h1>Extrato De Transações</h1>
+    <?php
+// Consulta SQL para buscar as transações
+$sql = "
+SELECT 
+    c.nome AS nome_credito, 
+    d.nome AS nome_debito, 
+    e.valor, 
+    e.mensagem 
+FROM banco_extrato e
+INNER JOIN usuarios c ON e.user_c = c.id
+INNER JOIN usuarios d ON e.user_d = d.id
+WHERE e.user_c = ? OR e.user_d = ?
+ORDER BY e.id DESC -- Adicione esta linha para ordenar por data em ordem decrescente
+LIMIT 10 -- Adicione esta linha para limitar o resultado a 10 registros
+";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('ii', $_SESSION['id'], $_SESSION['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Crédito</th><th>Débito</th><th>Valor</th><th>Mensagem</th></tr>";
+    // Saída dos dados de cada linha
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row['nome_credito'] . "</td><td>" . $row['nome_debito'] . "</td><td>" . $row['valor'] . "</td><td>" . $row['mensagem'] . "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhuma transação encontrada";
+}
+?>
+
+</div><!--form-transferir-->
+</div><!--duas primeiras-->
+
+    </div><!--tab4-->
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
